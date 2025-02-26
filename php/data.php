826fedee951e55ca3890317ae9654e330e73abe0
mabuw <nguyenhuycanh2505@gmail.com>
2025-02-26 17:05:50 +0700

    fix avatar user

--- php/data.php
+++ php/data.php
@@ -1,26 +1,30 @@
 <?php
     while($row = mysqli_fetch_assoc($query)){
-        $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
-                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
-                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+        $sql2 = "SELECT * FROM messages 
+                 WHERE (incoming_msg_id = {$row['unique_id']} OR outgoing_msg_id = {$row['unique_id']}) 
+                 AND (outgoing_msg_id = {$outgoing_id} OR incoming_msg_id = {$outgoing_id}) 
+                 ORDER BY msg_id DESC LIMIT 1";
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
-        (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
-        (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
+        
+        $result = (mysqli_num_rows($query2) > 0) ? $row2['msg'] : "No message available";
+        $msg = (strlen($result) > 28) ? substr($result, 0, 28) . '...' : $result;
+
         if(isset($row2['outgoing_msg_id'])){
-            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+            $you = ($outgoing_id == $row2['outgoing_msg_id']) ? "You: " : "";
         }else{
             $you = "";
         }
-        ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
-        ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
+        
+        $offline = ($row['status'] == "Offline now") ? "offline" : "";
+        $hid_me = ($outgoing_id == $row['unique_id']) ? "hide" : "";
 
         $output .= '<a href="chat.php?user_id='. $row['unique_id'] .'">
                     <div class="content">
                     <img src="../php/images/'. $row['img'] .'" alt="">
                     <div class="details">
                         <span>'. $row['fname']. " " . $row['lname'] .'</span>
-                        <p>'. $you . $msg .'</p>
+                        <p>'. $you . htmlspecialchars($msg) .'</p>
                     </div>
                     </div>
                     <div class="status-dot '. $offline .'"><i class="fas fa-circle"></i></div>
--- php/get-chat.php
+++ php/get-chat.php
@@ -1,37 +1,47 @@
 <?php 
-    session_start();
-    if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
-        $outgoing_id = $_SESSION['unique_id'];
-        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $output = "";
-        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.outgoing_msg_id
-                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
-                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
-        $query = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($query) > 0){
-            while($row = mysqli_fetch_assoc($query)){
-                if($row['outgoing_msg_id'] === $outgoing_id){
-                    $output .= '<div class="chat outgoing">
-                                <div class="details">
-                                    <p>'. $row['msg'] .'</p>
-                                </div>
-                                </div>';
-                }else{
-                    $output .= '<div class="chat incoming">
-                                <img src="../php/images/'.$row['img'].'" alt="">
-                                <div class="details">
-                                    <p>'. $row['msg'] .'</p>
-                                </div>
-                                </div>';
-                }
-            }
-        }else{
-            $output .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
-        }
-        echo $output;
-    }else{
-        header("location: ../login.php");
-    }
+session_start();
+if(isset($_SESSION['unique_id'])){
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
+    $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+    $output = "";
 
+    $sql = "SELECT messages.*, 
+                   sender.img AS sender_img, sender.unique_id AS sender_id, 
+                   receiver.img AS receiver_img, receiver.unique_id AS receiver_id 
+            FROM messages
+            LEFT JOIN users AS sender ON sender.unique_id = messages.outgoing_msg_id
+            LEFT JOIN users AS receiver ON receiver.unique_id = messages.incoming_msg_id
+            WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id}) 
+               OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) 
+            ORDER BY timestamp ASC";
+
+    $query = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($query) > 0){
+        while($row = mysqli_fetch_assoc($query)){
+            // Lấy đúng ảnh của người gửi
+            $user_img = ($row['outgoing_msg_id'] == $outgoing_id) ? $row['sender_img'] : $row['receiver_img'];
+
+            if($row['outgoing_msg_id'] === $outgoing_id){
+                $output .= '<div class="chat outgoing">
+                            <div class="details">
+                                <p>'. htmlspecialchars($row['msg']) .'</p>
+                            </div>
+                            </div>';
+            }else{
+                $output .= '<div class="chat incoming">
+                            <img src="../php/images/'.$user_img.'" alt="">
+                            <div class="details">
+                                <p>'. htmlspecialchars($row['msg']) .'</p>
+                            </div>
+                            </div>';
+            }
+        }
+    }else{
+        $output .= '<div class="text">No messages are available. Once you send a message, they will appear here.</div>';
+    }
+    echo $output;
+}else{
+    header("location: ../login.php");
+}
 ?>
